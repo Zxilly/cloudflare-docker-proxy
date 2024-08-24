7fb869046008648ec6cf928d2f909af266dfaf82
Zxilly <31370133+Zxilly@users.noreply.github.com>
2024-08-25 02:29:13 +0800

    upddate

--- src/index.js
+++ src/index.js
@@ -7,25 +7,22 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zxilly.dev": dockerHub,
+  // "quay.libcuda.so": "https://quay.io",
+  // "gcr.libcuda.so": "https://gcr.io",
+  // "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
+  // "k8s.libcuda.so": "https://registry.k8s.io",
+  // "ghcr.libcuda.so": "https://ghcr.io",
+  // "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  // "ecr.libcuda.so": "https://public.ecr.aws",
 
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  // // staging
+  // "docker-staging.libcuda.so": dockerHub,
 };
 
 function routeByHosts(host) {
   if (host in routes) {
     return routes[host];
-  }
-  if (MODE == "debug") {
-    return TARGET_UPSTREAM;
   }
   return "";
 }
